00c39cb02f360ee4853d99de1e18ace8915fe98b
Nschorr1 <113535165+Nschorr1@users.noreply.github.com>
2022-09-17 20:04:12 +0300

    updated enter button

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ var x = "";
 function generator1()
 {
     clear_box();
-    var second = ["אלעזר" ,"חנניה" ,"נחמיה" ,"עזרא" ,"עזריה" ,"גבריאל" ,"רפאל" ,"שאול" ,"איתן" ,"יונה" ,"שמעון" ,"יהודה" ,"אלישע" ,"יהושע" ,"דניאל" ,"לאה" ,"רחל" ,"רבקה" ,"שרה" ,"נחמה" ,"דוד" ,"אהרון" ,"משה" ,"שלמה" ,"יונתן" ,"יעקב" ,"יצחק" ,"אברהם" ,"ישראל", "יוסף", "ראובן", "יששכר", "נפטלי", "דן", "בנימין", "מנשה", "אפרים", "עקיבא", "אסתר"];
+    var second = ["אלעזר" ,"חנניה" ,"נחמיה" ,"עזרא" ,"עזריה" ,"גבריאל" ,"רפאל" ,"שאול" ,"איתן" ,"יונה" ,"שמעון" ,"יהודה" ,"אלישע" ,"יהושע" ,"דניאל" ,"לאה" ,"רחל" ,"רבקה" ,"שרה" ,"נחמה" ,"דוד" ,"אהרון" ,"משה" ,"שלמה" ,"יונתן" ,"יעקב" ,"יצחק" ,"אברהם" ,"ישראל", "יוסף", "ראובן", "יששכר", "נפתלי", "דן", "בנימין", "מנשה", "אפרים", "עקיבא", "אסתר"];
     var first = ["אור" ,"צדקת" ,"רצון" ,"תיקון" ,"נצח" ,"שירת" ,"תשועת" ,"מנחת" ,"זכרון" ,"אהבת" ,"נר" ,"תורת" ,"בית" ,"שלוות" ,"דביר" ,"שיח" ,"בני" ,"שמחת" ,"רנת" ,"תפילת" ,"מצוות" ,"אוהל" ,"היכל", "דרכי"];
     var first_random = Math.floor(Math.random() * first.length);
     var second_random = Math.floor(Math.random() * second.length);
@@ -15,14 +15,16 @@ function generator1()
     x = second[second_random];
 }
 
-function generator2(f_name,l_name)
+function generator2(f_name)
 {
     var first = ["אור" ,"צדקת" ,"רצון" ,"תיקון" ,"נצח" ,"שירת" ,"תשועת" ,"מנחת" ,"זכרון" ,"אהבת" ,"נר" ,"תורת" ,"בית" ,"שלוות" ,"דביר" ,"שיח" ,"בני" ,"שמחת" ,"רנת" ,"תפילת" ,"מצוות" ,"אוהל" ,"היכל", "דרכי"];
     var first_random = Math.floor(Math.random() * first.length);
     document.getElementById("eng").innerHTML = ('Generated shul name: ');
-    document.getElementById("result").innerHTML = (first[first_random] + ' ' + f_name + " ע״ש " + f_name + " " + l_name);
+    document.getElementById("result").innerHTML = (first[first_random] + ' ' + f_name);
     document.getElementById("result").style.fontSize = "x-large";
     document.getElementById("eng").style.fontSize = "x-large";
+    document.getElementById("donor").style.fontSize = "x-large";
+    x = f_name;
 }
 
 
@@ -41,11 +43,15 @@ function name_after()
 
 function choose_name()
 {
-    document.getElementById('donor').innerHTML = "";
-    x = "";
     var f_name = document.getElementById("f_name").value;
     var l_name = document.getElementById("l_name").value;
-    generator2(f_name,l_name);
+    if (f_name == ""|| l_name == "")
+    {
+        alert("Error: please enter a name first!");
+        return 0;
+    }
+    document.getElementById('donor').innerHTML = f_name + " " + l_name;
+    generator2(f_name);
 }
 
 
