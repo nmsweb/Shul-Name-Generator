4ac657978a648d05ffc7afa6b7fe1287e2cd91de
Nschorr1 <113535165+Nschorr1@users.noreply.github.com>
2022-09-18 15:07:42 +0300

    updated language settings and more names

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ function generator1()
 {
     clear_box();
     var second = ["אלעזר" ,"חנניה" ,"נחמיה" ,"עזרא" ,"עזריה" ,"גבריאל" ,"רפאל" ,"שאול" ,"איתן" ,"יונה" ,"שמעון" ,"יהודה" ,"אלישע" ,"יהושע" ,"דניאל" ,"לאה" ,"רחל" ,"רבקה" ,"שרה" ,"נחמה" ,"דוד" ,"אהרון" ,"משה" ,"שלמה" ,"יונתן" ,"יעקב" ,"יצחק" ,"אברהם" ,"ישראל", "יוסף", "ראובן", "יששכר", "נפתלי", "דן", "בנימין", "מנשה", "אפרים", "עקיבא", "אסתר"];
-    var first = ["אור" ,"צדקת" ,"רצון" ,"תיקון" ,"נצח" ,"שירת" ,"תשועת" ,"מנחת" ,"זכרון" ,"אהבת" ,"נר" ,"תורת" ,"בית" ,"שלוות" ,"דביר" ,"שיח" ,"בני" ,"שמחת" ,"רנת" ,"תפילת" ,"מצוות" ,"אוהל" ,"היכל", "דרכי"];
+    var first = ["אור" ,"צדקת" ,"רצון" ,"תיקון" ,"נצח" ,"שירת" ,"תשועת" ,"מנחת" ,"זכרון" ,"אהבת" ,"נר" ,"תורת" ,"בית" ,"שלוות" ,"דביר" ,"שיח" ,"בני" ,"שמחת" ,"רנת" ,"תפילת" ,"מצוות" ,"אוהל" ,"היכל", "דרכי", "קרן", "שושנת", "רינת"];
     var first_random = Math.floor(Math.random() * first.length);
     var second_random = Math.floor(Math.random() * second.length);
     document.getElementById("eng").innerHTML = ('Generated shul name: ');
@@ -17,7 +17,7 @@ function generator1()
 
 function generator2(f_name)
 {
-    var first = ["אור" ,"צדקת" ,"רצון" ,"תיקון" ,"נצח" ,"שירת" ,"תשועת" ,"מנחת" ,"זכרון" ,"אהבת" ,"נר" ,"תורת" ,"בית" ,"שלוות" ,"דביר" ,"שיח" ,"בני" ,"שמחת" ,"רנת" ,"תפילת" ,"מצוות" ,"אוהל" ,"היכל", "דרכי"];
+    var first = ["אור" ,"צדקת" ,"רצון" ,"תיקון" ,"נצח" ,"שירת" ,"תשועת" ,"מנחת" ,"זכרון" ,"אהבת" ,"נר" ,"תורת" ,"בית" ,"שלוות" ,"דביר" ,"שיח" ,"בני" ,"שמחת" ,"רנת" ,"תפילת" ,"מצוות" ,"אוהל" ,"היכל", "דרכי", "קרן", "שושנת", "רינת"];
     var first_random = Math.floor(Math.random() * first.length);
     document.getElementById("eng").innerHTML = ('Generated shul name: ');
     document.getElementById("result").innerHTML = (first[first_random] + ' ' + f_name);
@@ -45,6 +45,16 @@ function choose_name()
 {
     var f_name = document.getElementById("f_name").value;
     var l_name = document.getElementById("l_name").value;
+    if(!(/^[\u05D0-\u05EA + ֿ\u05F3]+$/.test(f_name)))
+    {
+        alert("Error: please enter a name in hebrew!");
+        return 0;
+    }
+    if(!(/^[\u05D0-\u05EA + ֿ\u05F3]+$/.test(l_name)))
+    {
+        alert("Error: please enter a name in hebrew!");
+        return 0;
+    }
     if (f_name == ""|| l_name == "")
     {
         alert("Error: please enter a name first!");
